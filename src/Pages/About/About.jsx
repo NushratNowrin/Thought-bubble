12259568385a7fa7545450a8cdbaa9362c31edcb
NushratNowrin <nowrin2023@gmail.com>
2024-01-04 19:03:03 +0600

    about and services page banner

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -6,11 +6,14 @@ const About = () => {
 				{/* background Overlay */}
 				<div className='absolute top-0 bottom-0 right-0 left-0 bg-black bg-opacity-60 z-10'></div>
 				{/* Breadcrumbs */}
-				<div className='relative z-20 flex flex-col items-center gap-0 pt-[20vh]'>
-					<h3 className='text-white text-6xl leading-none'>Contact</h3>
+				<div className='breadcrumbs relative z-20 flex flex-col items-center gap-0 pt-[22vh]'>
+					<h3 className='text-white text-[56px] leading-none'>About</h3>
 					<br />
-					<p className='text-white font-medium -my-5'>
-						Home / <span className='text-amber-400'>Contact</span>
+					<p className='text-white font-[500] -my-5 tracking-wide'>
+						<a href='/' className='no-underline hover:underline'>
+							Home
+						</a>{" "}
+						/ <span className='text-amber-400'>About</span>
 					</p>
 				</div>
 			</div>
