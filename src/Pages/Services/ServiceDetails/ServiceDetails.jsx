21226a382cb0211fd57f48949813ee72bd2b3598
NushratNowrin <nowrin2023@gmail.com>
2024-01-06 02:37:55 +0600

    Service details page banner done

--- src/Pages/Services/ServiceDetails/ServiceDetails.jsx
+++ src/Pages/Services/ServiceDetails/ServiceDetails.jsx
@@ -1,9 +1,22 @@
 import { useLoaderData } from "react-router-dom";
+import Banner from "../../../Utilities/Banner/Banner";
 
 const ServiceDetails = () => {
-	const jobData = useLoaderData();
-	console.log(jobData);
-	return <div>ServiceDetails</div>;
+	const serviceData = useLoaderData();
+	const { banner_img, title } = serviceData;
+	console.log(serviceData);
+	return (
+		<div>
+			{/* Services Banner */}
+			<div>
+				<Banner
+					bannerBg={banner_img}
+					heading={title}
+					breadcrumbs={"Home"}
+					breadcrumbsUrl={"/"}></Banner>
+			</div>
+		</div>
+	);
 };
 
 export default ServiceDetails;
--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -1,23 +1,18 @@
+import servicesBg from "../../assets/services/services.jpg";
 import { services } from "../../Contents";
+import Banner from "../../Utilities/Banner/Banner";
 import Service from "./Service/Service";
 import "./Services.css";
 const Services = () => {
 	return (
 		<div>
-			<div className='services-hero relative'>
-				{/* background Overlay */}
-				<div className='absolute top-0 bottom-0 right-0 left-0 bg-black bg-opacity-60 z-10'></div>
-				{/* Breadcrumbs */}
-				<div className='breadcrumbs relative z-20 flex flex-col items-center gap-0 pt-[22vh]'>
-					<h3 className='text-white text-[56px] leading-none'>Services</h3>
-					<br />
-					<p className='text-white font-[500] -my-5 tracking-wide'>
-						<a href='/' className='no-underline hover:underline'>
-							Home
-						</a>{" "}
-						/ <span className='text-amber-400'>Services</span>
-					</p>
-				</div>
+			{/* Services Banner */}
+			<div>
+				<Banner
+					bannerBg={servicesBg}
+					heading={"Services"}
+					breadcrumbs={"Home"}
+					breadcrumbsUrl={"/"}></Banner>
 			</div>
 			{/* Services body */}
 			<div className='bg-[#f2dab8] z-10 p-20 pb-60'>
